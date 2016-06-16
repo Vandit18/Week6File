9547ca0759d479f7ea5598d32c2c1afa46e944b4
vandit18 <vanditkothari@gmail.com>
2016-06-16 14:18:31 -0400

    Add project files.

---
+++ COMP2007-S2016-Lesson6B/Register.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+//required for identity and OWIN CODE FIRST APPROACH
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
+
+namespace COMP2007_S2016_Lesson6B
+{
+    public partial class Register : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void CancelButton_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Default.aspx");
+        }
+
+        protected void RegisterButton_Click(object sender, EventArgs e)
+        {
+            //create a new user store and user manager objects.
+            var userStore = new UserStore<IdentityUser>();
+            var userManager = new UserManager<IdentityUser>(userStore);
+
+            //create new user
+            var user = new IdentityUser()
+            {
+                //initialize syntex.
+                UserName = UserNameTextBox.Text,
+                PhoneNumber = PhoneNumberTextBox.Text,
+                Email = EmailTextBox.Text
+
+            };
+
+
+
+            //get result of cretated user
+            IdentityResult result = userManager.Create(user, PasswordTextBox.Text);//password hash
+
+            //check if user creation succeded
+            if (result.Succeeded)
+            {
+                var authenticationManager = HttpContext.Current.GetOwinContext().Authentication;
+                var userIdentity = userManager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
+                authenticationManager.SignIn(new AuthenticationProperties() { }, userIdentity);
+
+                Response.Redirect("~/MainMenu.aspx");
+
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
